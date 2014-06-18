36325e923b6abbec565bfeb75b24146179b567fd
deanrussell <deanwagnerrussell@gmail.com>
2014-06-17 22:16:19 -0400

    smooth toggle animation

--- js/sife.js
+++ js/sife.js
@@ -1,11 +1,21 @@
-// JavaScript for sife!
+// jQuery for sife!
 
 $(document).ready(function(){
-	$("#notes-buttonID").click(function(){
+	$("#notes-buttonID, #arrowId").click(function(){
         $("#panel-coverId").toggleClass("move-panel-cover");
         $("#panel-main__innerId").toggleClass("move-panel-main__inner");
+        $("#arrowId").toggleClass("arrow__wrapper");
     });
+    $("#arrowId").hover(function(){
+    	$("#arrow__aId").toggleClass("arrow_border");
+    })
 });
+
 
 /* var d = document.getElementById("panel-coverId");
 d.className = d.className + " move-panel-cover"; */
+
+/*transform: 'translateX(-50%)',
+        	WebkitTransform: 'translateX(-50%)',
+        	transition: 'all 0.75 ease-in',
+        	WebkitTransition: 'all 0.75 ease-in'*/
